c6bca6d835824ffbe89993fd45a82a8b797088a4
gjylt <gjylt520@163.com>
2018-11-02 17:49:07 +0800

    Add files via upload

--- runing.py
+++ runing.py
@@ -10,6 +10,28 @@ import tkinter as tk
 from tkinter import filedialog
 import json
 
+
+
+def dirlist(path,tags = ['.avi','.mp4'] ,TfRecusive = False):
+    filelist = os.listdir(path)
+    result = []
+    for filename in filelist:
+        filepath = os.path.join(path, filename)
+        if os.path.isdir(filepath):
+
+            if TfRecusive == False:
+                continue
+
+            tmp = dirlist(filepath, tags,TfRecusive)
+            result = result +tmp
+        else:
+            for tag in tags:
+                if os.path.splitext(filename)[1] == tag:
+                    result.append(filepath)
+
+    return result
+
+
 #这个窗口继承了用 QtDesignner 绘制的窗口
 class mywindow(QTabWidget,Ui_TabWidget):
 
@@ -25,95 +47,62 @@ class mywindow(QTabWidget,Ui_TabWidget):
         self.th.changePixmap.connect(self.setImage)
         self.th.changeframeC.connect(self.ChangeSliderValue)
         self.th.changeslideMax.connect(self.ChangeSliderMax)
-
         #self.slider.valueChanged.connect(self.SliderValueChanged)
         self.slider.sliderMoved.connect(self.SliderValueChanged)
-
         self.current_play_videoname = ""
         self.annotation_dri = './'
 
+        self.list.itemDoubleClicked.connect(self.listclicked)
+
+
+
     def paintEvent(self, evt):
-        #print("paint")
-        #print(self.size())
         tmp = 0
 
     def resizeEvent(self, QResizeEvent):
-
-
-        w = self.size().width()
-        h = self.size().height()
-
-
-        #self.label.resize( QtCore.QSize( int(w*self.label_width), int(h*self.label_hight)))
-
-        label_h = int(h*self.label_h_ration)
-        label_w = int(w*self.label_w_ration)
-        self.label.setGeometry(QtCore.QRect(110, 30,  label_w, label_h ))
-
-        self.slider.setGeometry(110, label_h+30+30, label_w, 30)
-
-        l_h = self.label.size().width()
-        l_w = self.label.size().height()
-
-        l_h = l_h * 1.0 / h
-        l_w = l_w*1.0/w
-
-        print(l_h,self.label_h_ration,l_w,self.label_w_ration )
-
-
+        self.recalc_layout(self)
         print("resize")
-
-
 
     def ChangeSliderMax(self,max):
         self.slider.setMaximum(max)
         self.frameMax = max
 
     def SliderValueChanged( self, value ):
-
         self.slidervalue = value
         self.th.changeframe(value)
         time.sleep(1.0/30)
         self.th.move_slider = True
-        #print("SliderValueChanged", value)
-
 
     def ChangeSliderValue( self, value ):
-
-        #if (self.slider.sliderPressed() == False):
-
         self.slider.setValue(int(value))
 
+    def setCurrentPlayVideoPath(self,videoName):
 
-        #print("ChangeSliderValue",value)
+        if self.current_play_videoname != "":
+            self.saverannotation()
 
-
+        if videoName != "":
+            self.current_play_videoname = videoName
+            self.th.setVideoName(videoName)
+            self.th.start()
 
     def videoprocessing(self):
 
         print("gogo")
 
-        if self.current_play_videoname != "":
-            self.saverannotation()
 
         videoName,videoType= QFileDialog.getOpenFileName(self,
                                                         "打开视频",
                                                          "./avi",
-                                                            "*.avi;;*.mp4;;All Files (*)"
+                                                            "*.mp4;;*.avi;;All Files (*)"
                                                          )
 
-        if videoName != "":
-            self.current_play_videoname = videoName
-            self.th.setVideoName(videoName)
-
-            self.th.start()
+        self.setCurrentPlayVideoPath(videoName)
 
 
     def setImage(self, image):
         #sz = self.label.shape
         self.label.setPixmap(QPixmap.fromImage(image))
-
-
 
     def ReplayVideo(self):
         self.th.StopPlay = True
@@ -123,7 +112,6 @@ class mywindow(QTabWidget,Ui_TabWidget):
         self.th.start()
 
     def keyPressEvent(self, event):
-
         # 这里event.key（）显示的是按键的编码
         #print("按下：" + str(event.key())) # 举例，这里Qt.Key_A注意虽然字母大写，但按键事件对大小写不敏感
         if (event.key() == Qt.Key_S):
@@ -135,7 +123,6 @@ class mywindow(QTabWidget,Ui_TabWidget):
         if (event.key() == Qt.Key_D):
             self.th.delList()
             print('删除标签')
-
         if (event.key() == Qt.Key_I):
             self.th.change_frame_step( True)
             print('增加')
@@ -148,27 +135,54 @@ class mywindow(QTabWidget,Ui_TabWidget):
         if (event.key() == Qt.Key_J):
             self.th.step_change_frame( False)
             print('后退')
-
         if (event.key() == Qt.Key_Enter):
             print('测试：Enter')
         if (event.key() == Qt.Key_Space):
             print('测试：Space')
 
     def saverannotation(self):
-        self.th.StopPlay = True
-
+        #self.th.StopPlay = True
         name = self.current_play_videoname.split('/')[-1].split('.')[0]
-        pth = self.annotation_dri + name + '.json'
+        pth = os.path.join(self.annotation_dri, name + '.json')
         self.th.save_json(pth)
-        time.sleep(0.1)
-        self.th.StopPlay = False
+        #time.sleep(0.1)
+        #self.th.StopPlay = False
 
-    def setsavepath(self):
-
+    def SetVideoDri(self):
         file_path = QFileDialog.getExistingDirectory(self, "选择文件夹", "./")
 
+        filelist = dirlist(file_path, tags=['.avi', '.mp4'], TfRecusive=False)
+
+        self.dict_file = {}
+        for file in filelist:
+            filename = file.split('/')[-1].split('.')[0]
+            self.dict_file[filename] = file
+            self.list.addItem(filename)
+
+    def listclicked(self,item):
+
+        pth = self.dict_file[item.text()]
+        self.setCurrentPlayVideoPath(pth)
+        self.list.clearFocus()
+        print("clicked", item.text(),pth)
+
+        #self.clicked()
+
+
+    def setsavepath(self):
+        file_path = QFileDialog.getExistingDirectory(self, "选择文件夹", "./")
+        self.th.set_annotation_dir(file_path)
         self.annotation_dri = file_path
         #print(self.annotation_dri)
+
+    def VideoPlayPause(self):
+        _translate = QtCore.QCoreApplication.translate
+        if self.th.video_play_pause:
+            self.th.video_play_pause = False
+            self.pushButton_5.setText(_translate("TabWidget", "暂停"))
+        else:
+            self.th.video_play_pause = True
+            self.pushButton_5.setText(_translate("TabWidget", "继续"))
 
 
 
@@ -187,7 +201,11 @@ class Thread(QThread):
     video_play_pause = False
     frame_step = 1
     move_slider = False
+    framerate = 30
+    time_duration = 0
 
+    def set_annotation_dir(self,pth):
+        self.annotation_dri = pth
 
     def load_json(self,pth):
 
@@ -195,24 +213,28 @@ class Thread(QThread):
         model = json.load(fr)
         fr.close()
 
-        return model
+        return model['annotation']
 
 
 
     def save_json(self,pth):
+        dict = {}
+        dict['framenum']     = self.framenum
+        dict['fps']          = self.framerate
+        #dict['timeduration'] = self.time_duration
+        dict['annotation']   = self.rangelist
 
-        if len(self.rangelist) > 0:
-            jsObj = json.dumps(self.rangelist)
-            with open(pth, "w") as fw:
-                fw.write(jsObj)
-            fw.close()
+        jsObj = json.dumps(dict)
+        with open(pth, "w") as fw:
+            fw.write(jsObj)
+        fw.close()
 
         #
     def run(self):
         cap             = cv2.VideoCapture(self.videoname)
         self.framenum   = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        framerate       = 30 #int(cap.get(cv2.CAP_PROP_FPS))
-        cap.set(cv2.CAP_PROP_FPS, framerate)
+        self.framerate       = int(cap.get(cv2.CAP_PROP_FPS))
+        self.time_duration = cap.get( cv2.CAP_PROP_OPENNI_MAX_TIME_DURATION )
 
 
         self.changeslideMax.emit( self.framenum )
@@ -220,7 +242,8 @@ class Thread(QThread):
 
         name = self.videoname.split('/')[-1].split('.')[0]
 
-        pth = self.annotation_dri + name + '.json'
+        pth = os.path.join(self.annotation_dri , name + '.json')
+        self.rangelist = []
         if os.path.exists(pth) and os.path.isfile(pth):
             self.rangelist =  self.load_json(pth)
 
@@ -233,16 +256,9 @@ class Thread(QThread):
             while(self.video_play_pause):
                 tmp = 1
 
-            #print(self.frame_c)
-
             if self.move_slider:
                 cap.set(cv2.CAP_PROP_POS_FRAMES, self.frame_c )
                 self.move_slider = False
-
-            #videoframe = cap.get(cv2.CAP_PROP_POS_FRAMES)
-
-            #print("current video frame",videoframe,self.frame_c )
-
             ret, frame   = cap.read()
 
             if ret:
@@ -257,9 +273,16 @@ class Thread(QThread):
                 for rang in self.rangelist:
                     tmp = [i for i in range( int(shp[1]*rang[0]), int(shp[1]*rang[1]))]
                     tmp1 = tmp + tmp1
-                frame[int(0.9*shp[0]):shp[0],tmp1,1] = 255
 
+                frame[int(0.9*shp[0]):shp[0],tmp1,2] = 255 #annotation
 
+                # current frame
+                cur_frame_idx = int(value * shp[1])
+                frame[int(0.9 * shp[0]):shp[0], cur_frame_idx, 1] = 255
+
+                print(cur_frame_idx)
+
+                #print frame step
                 cv2.putText(frame, 'step:'+str(self.frame_step), (20, 20), cv2.FONT_HERSHEY_PLAIN, 2.0, (255, 0, 0), 2, 1)
 
                 rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -268,18 +291,22 @@ class Thread(QThread):
 
                 #发送图片 刷新显示
                 self.changePixmap.emit(p)
+            else:
+                continue
 
 
-            time.sleep(1.0/framerate) #控制视频播放的速度
+            time.sleep(1.0/self.framerate) #控制视频播放的速度
 
             self.frame_c = self.frame_c + 1
+            if self.frame_c > self.framenum:
+                self.frame_c = self.framenum -1
             # 改变 slider 进度
             self.changeframeC.emit(self.frame_c)
 
         cap.release()
 
         name = self.videoname.split('/')[-1].split('.')[0]
-        pth = self.annotation_dri + name + '.json'
+        pth = os.path.join(self.annotation_dri, name + '.json')
         self.save_json(pth)
 
 
--- x.py
+++ x.py
@@ -1,61 +1,86 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+
+
 class Ui_TabWidget(object):
     def setupUi(self, TabWidget):
 
         TabWidget.setObjectName("TabWidget") #创建的是"TabWidget"
         TabWidget.resize(789, 619)
-        #TabWidget.setScaledContents(True)
 
-
-        self.lef_side   = 75
-        self.right_side = 75
-
+        #############################################
+        self.lef_side = 110
+        self.right_side = 110
+        self.top_side = 30
+        self.bottom_side = 30
+        self.slider_height = 30
+        self.button_width = 75
+        self.button_height = 23
+        self.slider_label_blank = 5
 
         self.main_w = self.size().width()
         self.main_h = self.size().height()
 
-        # "第一个子窗口"
-        self.tab = QtWidgets.QWidget()
+        # button
+        self.button_x = 10
 
-        self.tab.setObjectName("tab")
+        # label
+        self.label_x = self.lef_side
+        self.label_y = self.top_side
+        self.label_width = self.main_w - self.lef_side - self.right_side
+        self.label_height = self.main_h - self.slider_height - self.slider_label_blank
+
+        # slider
+        self.slider_x = self.lef_side
+        self.slider_y = self.main_h - self.bottom_side - self.slider_height
+        self.slider_width = self.label_width
+
+        ################################
+
+
+
+
+        # "第一个子窗口"
+        # self.tab = QtWidgets.QWidget()
+        #
+        # self.tab.setObjectName("tab")
+
+        #list widget
+        self.list = QtWidgets.QListWidget(TabWidget)
+        self.setFocusPolicy(QtCore.Qt.NoFocus)
+
 
         self.pushButton = QtWidgets.QPushButton(TabWidget)
-        self.pushButton.setGeometry(QtCore.QRect(10, 30, 75, 23))
         self.pushButton.setObjectName("pushButton")
 
         self.pushButton_2 = QtWidgets.QPushButton(TabWidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(10, 90, 75, 23))
         self.pushButton_2.setObjectName("pushButton_2")
 
         self.pushButton_3 = QtWidgets.QPushButton(TabWidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(10, 150, 75, 23))
         self.pushButton_3.setObjectName("pushButton_3")
 
-
         self.pushButton_4 = QtWidgets.QPushButton(TabWidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(10, 210, 75, 23))
         self.pushButton_4.setObjectName("pushButton_4")
 
-        self.label_h_ration = 0.8
-        self.label_w_ration = 0.8
-        lab_h = int(self.main_h*self.label_h_ration)
-        lab_w = int(self.label_w_ration*self.main_w)
+        self.pushButton_5 = QtWidgets.QPushButton(TabWidget)
+        self.pushButton_5.setObjectName("pushButton_5")
+
+        self.pushButton_6 = QtWidgets.QPushButton(TabWidget)
+        self.pushButton_6.setObjectName("pushButton_6")
+
         self.label = QtWidgets.QLabel(TabWidget)
-        self.label.setGeometry(QtCore.QRect(110, 30, lab_w, lab_h ))
+        #self.label.setGeometry(QtCore.QRect(self.label_x, self.label_y, self.label_width, self.label_height))
         self.label.setScaledContents(True)
         self.label.setText("")
         self.label.setObjectName("label")
 
-
         self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal, TabWidget)
         self.slider.setFocusPolicy(QtCore.Qt.NoFocus)
         self.slider.setSingleStep(1)
-        self.slider.setGeometry(110, lab_h +40, lab_w, 30)
+        #self.slider.setGeometry(self.slider_x, self.slider_y, self.slider_width, self.slider_height)
 
 
 
-
-        #TabWidget.addTab(self.tab, "")
+        #self.recalc_layout(self)
 
 
         self.retranslateUi(TabWidget)
@@ -68,8 +93,65 @@ class Ui_TabWidget(object):
         self.pushButton_2.clicked.connect(TabWidget.videoprocessing)
         self.pushButton_3.clicked.connect(TabWidget.saverannotation)
         self.pushButton_4.clicked.connect(TabWidget.setsavepath)
+        self.pushButton_5.clicked.connect(TabWidget.VideoPlayPause)
+        self.pushButton_6.clicked.connect(TabWidget.SetVideoDri)
 
         QtCore.QMetaObject.connectSlotsByName(TabWidget)
+
+    def recalc_layout(self, TabWidget):
+        ############################
+
+        self.lef_side    = 110
+        self.right_side  = 200
+        self.top_side    = 30
+        self.bottom_side = 30
+        self.slider_height    = 30
+        self.button_width = 75
+        self.button_height = 23
+        self.slider_label_blank = 5
+
+        self.main_w      = self.size().width()
+        self.main_h      = self.size().height()
+
+        #button
+        self.button_x = 10
+
+        #label
+        self.label_x = self.lef_side
+        self.label_y = self.top_side
+        self.label_width = self.main_w - self.lef_side - self.right_side
+        self.label_height= self.main_h - self.slider_height - self.slider_label_blank - self.top_side - self.bottom_side
+
+        #slider
+        self.slider_x = self.lef_side
+        self.slider_y = self.main_h - self.bottom_side - self.slider_height
+        self.slider_width = self.label_width
+
+        #list
+        self.list_x = self.lef_side + self.label_width + 9
+        self.list_y = self.top_side
+        self.list_width = self.right_side-20
+        self.list_height= int(self.label_height*0.8)
+
+        ################################
+
+        self.pushButton.setGeometry(QtCore.QRect(self.button_x, 30, self.button_width, self.button_height))
+
+        self.pushButton_2.setGeometry(QtCore.QRect(self.button_x, 90, self.button_width, self.button_height))
+
+        self.pushButton_3.setGeometry(QtCore.QRect(self.button_x, 150, self.button_width, self.button_height))
+
+        self.pushButton_4.setGeometry(QtCore.QRect(self.button_x, 210, self.button_width, self.button_height))
+
+        self.pushButton_5.setGeometry(QtCore.QRect(self.button_x, 270, self.button_width, self.button_height))
+
+        self.pushButton_6.setGeometry(QtCore.QRect(self.button_x, 330, self.button_width, self.button_height))
+
+        self.label.setGeometry(QtCore.QRect( self.label_x, self.label_y, self.label_width, self.label_height ))
+
+        self.slider.setGeometry( self.slider_x, self.slider_y, self.slider_width, self.slider_height )
+
+        self.list.setGeometry( self.list_x, self.list_y, self.list_width, self.list_height )
 
     def retranslateUi(self, TabWidget):
         _translate = QtCore.QCoreApplication.translate
@@ -78,6 +160,8 @@ class Ui_TabWidget(object):
         self.pushButton_2.setText(_translate("TabWidget", "打开视频"))
         self.pushButton_3.setText(_translate("TabWidget", "保存标签"))
         self.pushButton_4.setText(_translate("TabWidget", "标签路径"))
+        self.pushButton_5.setText(_translate("TabWidget", "暂停"))
+        self.pushButton_6.setText(_translate("TabWidget", "视频列表"))
 
         #TabWidget.setTabText(TabWidget.indexOf(self.tab), _translate("TabWidget", "Tab 1"))
 
